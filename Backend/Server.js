cdb7fd93b5ed475cc3e55b4f2eb6fb4e0b5f60d3
Sandhish <sandhish19@gmail.com>
2025-04-12 12:10:38 +0530

    backend changes1

--- Backend/Server.js
+++ Backend/Server.js
@@ -10,15 +10,20 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors(
-    {
-        origin: "http://localhost:5173",
-        credentials: true,
-        methods: ["GET", "POST", "PUT", "DELETE"],
-        allowedHeaders: ["Content-Type", "Authorization"],
-    }
-)
-);
+app.use(
+    cors({
+      origin: "http://localhost:5173",
+      methods: ["GET", "POST", "DELETE", "PUT"],
+      allowedHeaders: [
+        "Content-Type",
+        "Authorization",
+        "Cache-Control",
+        "Expires",
+        "Pragma",
+      ],
+      credentials: true,
+    })
+  );
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
